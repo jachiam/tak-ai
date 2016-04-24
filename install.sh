aceb56e8c6a7993b77b059987e898f5af75e176a
Tobias Merkle <tgamerkle@gmail.com>
2016-04-23 19:52:16 -0700

    round out installer script

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
-#!/bin/sh
-git clone https://github.com/torch/distro.git ~/torch --recursive
+git clone https://github.com/torch/distro.git ~/torch --recursive;
 cd ~/torch; bash install-deps;
-./install.sh
-source ~/.zshrc
-source ~/.bashrc
-source ~/.profile
-apt-get install love 
-apt-get update
-apt-get upgrade
-luarocks install utf8
+./install.sh;
+apt-get -y install love lua5.2 luarocks;
+apt-get -y update;
+apt-get -y upgrade;
+luarocks install utf8;
+git clone https://github.com/asgardiator/tak-ai.git ~/TakAI --recursive;
+cd; echo "love ~/TakAI" >> 'PLAY_TAK.sh';
+love ~/TakAI;
